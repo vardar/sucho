6fae30076b1c1479f71338c53f09fdd418f5cfb5
Jaroslav Vido <jaroslav.vido@gmail.com>
2013-10-22 21:03:25 +0200

    innitial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,34 @@
+class PagesController < ApplicationController
+  def home
+  end	
+
+  def about_project
+  end
+
+  def lit_resources
+  end
+
+  def actual_weather
+  end
+
+  def citizens
+  end
+
+  def civildefense
+  end
+
+  def irrigation
+  end
+
+  def reporter
+  end
+
+  def contacts
+  end
+
+  def cv
+  end
+
+  def thanks
+  end
+end
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,4 @@
+class WelcomeController < ApplicationController
+  def info
+  end
+end
